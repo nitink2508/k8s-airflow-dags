cbde3a67cb15c11cabd40d832543655791380d5e
nitink2508 <156999986+nitink2508@users.noreply.github.com>
2024-01-20 23:37:21 +0530

    Create r-demo.py

---
+++ dags/r-demo.py
@@ -0,0 +1,39 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.operators.dummy_operator import DummyOperator
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime.utcnow(),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG(
+    'r_hello_world', default_args=default_args, schedule_interval=timedelta(minutes=10))
+
+
+start = DummyOperator(task_id='start', dag=dag)
+
+passing = KubernetesPodOperator(namespace='airflow',
+                          image="nitinkalyankerdev/r-demo:latest",
+                          cmds=["Rscript","script.R"],
+                          labels={"foo": "bar"},
+                          name="r-test",
+                          task_id="r-task",
+                          get_logs=True,
+                          image_pull_policy='Always',
+                          hostnetwork=True,
+                          is_delete_operator_pod=False,
+                          dag=dag
+                          )
+
+
+end = DummyOperator(task_id='end', dag=dag)
+
+start >> passing >> end
