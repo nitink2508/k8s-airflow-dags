d6676892ebd8e23842b138df32fb72af0aedbf45
nitink2508 <156999986+nitink2508@users.noreply.github.com>
2024-01-26 17:07:36 +0530

    Update r-demo.py

--- dags/r-demo.py
+++ dags/r-demo.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.models import Variable
-
+from kubernetes.client import models as k8s
 image_name = Variable.get("image")
 default_args = {
     'owner': 'airflow',
