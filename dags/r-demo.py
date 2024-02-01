7771ca8935a562f7652e3606b194e65a39a9bb5e
nitink2508 <156999986+nitink2508@users.noreply.github.com>
2024-02-01 16:28:17 +0530

    Update r-demo.py

--- dags/r-demo.py
+++ dags/r-demo.py
@@ -29,16 +29,25 @@ dag = DAG(
 #     name="test-volume",
 #     host_path=k8s.V1HostPathVolumeSource(path=local_scripts_path)
 # )
-local_scripts_path = "/opt/airflow/scripts"
+# local_scripts_path = "/opt/airflow/scripts"
+
+# volume_mount = k8s.V1VolumeMount(
+#     name="local-scripts-volume", mount_path=local_scripts_path, sub_path=None, read_only=True
+# )
+
+# volume = k8s.V1Volume(
+#     name="local-scripts-volume",
+#     persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name="local-scripts-pvc"),
+# )
 
 volume_mount = k8s.V1VolumeMount(
-    name="local-scripts-volume", mount_path=local_scripts_path, sub_path=None, read_only=True
+    name="test-volume", mount_path="/opt/airflow/scripts", sub_path=None, read_only=True
+)
+volume = k8s.V1Volume(
+    name="test-volume",
+    persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name="test-volume-claim"),
 )
 
-volume = k8s.V1Volume(
-    name="local-scripts-volume",
-    persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name="local-scripts-pvc"),
-)
 start = DummyOperator(task_id='start', dag=dag)
 
 # passing = KubernetesPodOperator(namespace='airflow',
