0f6f0598ccb6685a8a6664e3e0c15f7822abbe4f
nitink2508 <156999986+nitink2508@users.noreply.github.com>
2024-02-08 15:22:56 +0530

    Update r-demo.py

--- dags/r-demo.py
+++ dags/r-demo.py
@@ -45,7 +45,7 @@ volume_mount = k8s.V1VolumeMount(
 )
 volume = k8s.V1Volume(
     name="test-volume",
-    persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name="test-volume-claim"),
+    persistent_volume_claim=k8s.V1PersistentVolumeClaimVolumeSource(claim_name="pvc-www"),
 )
 
 start = DummyOperator(task_id='start', dag=dag)
@@ -62,24 +62,9 @@ start = DummyOperator(task_id='start', dag=dag)
 #                           hostnetwork=True,
 #                           dag=dag
 #                           )
-passing = KubernetesPodOperator(namespace='airflow',
-                          image=image_name,
-                          #cmds=["Rscript","script.R"],
-                          #image_pull_secrets="regcred",
-                          image_pull_secrets=[k8s.V1LocalObjectReference("regcred")],
-                          labels={"foo": "bar"},
-                          name="r-test",
-                          task_id="r-task",
-                          get_logs=True,
-                          image_pull_policy='Always',
-                          in_cluster=True,
-                          hostnetwork=True,
-                          dag=dag
-                          )
-
 # passing = KubernetesPodOperator(namespace='airflow',
 #                           image=image_name,
-#                           cmds=["Rscript","/opt/airflow/scripts/script.R"],
+#                           #cmds=["Rscript","script.R"],
 #                           #image_pull_secrets="regcred",
 #                           image_pull_secrets=[k8s.V1LocalObjectReference("regcred")],
 #                           labels={"foo": "bar"},
@@ -89,10 +74,25 @@ passing = KubernetesPodOperator(namespace='airflow',
 #                           image_pull_policy='Always',
 #                           in_cluster=True,
 #                           hostnetwork=True,
-#                           volumes=[volume],
-#                           volume_mounts=[volume_mount],
 #                           dag=dag
 #                           )
+
+passing = KubernetesPodOperator(namespace='airflow',
+                          image=image_name,
+                          cmds=["Rscript","/opt/airflow/scripts/script.R"],
+                          #image_pull_secrets="regcred",
+                          image_pull_secrets=[k8s.V1LocalObjectReference("regcred")],
+                          labels={"foo": "bar"},
+                          name="r-test",
+                          task_id="r-task",
+                          get_logs=True,
+                          image_pull_policy='Always',
+                          in_cluster=True,
+                          hostnetwork=True,
+                          volumes=[volume],
+                          volume_mounts=[volume_mount],
+                          dag=dag
+                          )
 
 
 
